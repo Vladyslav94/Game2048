bbb85bacf147f5186f4baef91e3ac1d71ac919c5
Vladyslav94 <grigoren_vlad2009@yahoo.com>
2020-04-15 21:28:48 +0300

    adding new tiles, reseting full field fetures

--- Model.java
+++ Model.java
@@ -1,4 +1,50 @@
 package Game2048;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Model {
+    private static final int FIELD_WIDTH = 4;
+    private Tile[][] gameTiles;
+
+    public Model() {
+        gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+        resetGameTiles();
+    }
+
+    public Tile[][] getGameTiles() {
+        return gameTiles;
+    }
+
+    private List<Tile> getEmptyTiles() {
+        List<Tile> list = new ArrayList<>();
+        for (int i = 0; i < FIELD_WIDTH; i++) {
+            for (int j = 0; j < FIELD_WIDTH; j++) {
+                if (gameTiles[i][j].isEmpty()) {
+                    list.add(gameTiles[i][j]);
+                }
+            }
+        }
+        return list;
+    }
+
+
+    private void addTile() {
+        if (getEmptyTiles().size() > 0){
+            getEmptyTiles().get((int)(getEmptyTiles().size() * Math.random())).value = (Math.random() < 0.9 ? 2 : 4);}
+    }
+
+    public void resetGameTiles() {
+        this.gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+        for (int i = 0; i < FIELD_WIDTH; i++) {
+            for (int j = 0; j < FIELD_WIDTH; j++) {
+                gameTiles[i][j] = new Tile();
+            }
+        }
+        addTile();
+        addTile();
+    }
+
+
 }
+
