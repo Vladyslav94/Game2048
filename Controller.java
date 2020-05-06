94326375ab5379f007f85f549da38f1c79b45a2a
Vladyslav94 <grigoren_vlad2009@yahoo.com>
2020-05-06 15:13:43 +0300

    Writing the View and adding KeyPressed method to Controller

--- Controller.java
+++ Controller.java
@@ -1,4 +1,54 @@
 package Game2048;
 
-public class Controller {
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class Controller extends KeyAdapter {
+    private Model model;
+    private View view;
+    private static int WINNING_TILE = 2048;
+
+    public Tile[][] getGameTiles(){
+        return model.getGameTiles();
+    }
+
+    public int getScore(){
+        return model.score;
+    }
+
+    public Controller(Model model){
+        this.model = model;
+        view = new View(this);
+    }
+
+    public void resetGame(){
+        model.score = 0;
+        view.isGameLost = false;
+        view.isGameWon = false;
+        model.resetGameTiles();
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
+        if(code == KeyEvent.VK_ESCAPE)
+            resetGame();
+        if(!model.canMove())
+            view.isGameLost = true;
+        if(!view.isGameLost && !view.isGameWon){
+            if(code == KeyEvent.VK_LEFT){
+                model.left();
+            } else if(code == KeyEvent.VK_RIGHT){
+                model.right();
+            } else if(code == KeyEvent.VK_UP){
+                model.up();
+            } else if(code == KeyEvent.VK_DOWN){
+                model.down();
+            }
+        }
+
+        if(model.maxTile == WINNING_TILE)
+            view.isGameWon = true;
+        view.repaint();
+    }
 }
